068f16db6867161ea39d90416903cff6941652be
adithya-57 <101978158+adithya-57@users.noreply.github.com>
2024-05-27 17:00:33 +0530

    Add files via upload

---
+++ adina-Copy1.py
@@ -0,0 +1,41 @@
+
+     a=(input())
+     d=float(input())
+     s=float(input())
+     t=d/s
+     print(t)
+     a2=(input())
+     d2=float(input())
+     s2=float(input())
+     t2=d2/s2
+     print(t2)
+   771.75
+   140
+   5.5125
+   
+    if(t<t2):
+        print("Time less so low efficiency")
+    else:
+        print("high efficiency")
+          771.75
+          180
+          4.2875
+    i=float(input())
+    j=float(input())
+    if(i<j):
+        print(i)
+    else:
+        print("out of range")
+    22.05
+    771.75
+    22.05 
+     i2=float(input())
+     j2=float(input())
+     if(i2<j2):
+        print(i)
+     else:
+         print("out of range")
+    756.85
+    700
+    out of range
+  
